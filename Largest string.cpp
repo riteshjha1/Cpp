5e7bb6f4cf02bf45a4afb881d2dd2fe2bd736111
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2021-10-02 21:35:50 +0530

    Create Largest string.cpp

---
+++ Largest string.cpp
@@ -0,0 +1,27 @@
+Question:Read N strings and print the largest string. Each string can have upto 1000 characters.
+
+
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    char sentence[1000];
+    cin.get();
+    char largest[1000];
+    int largest_len=0;
+
+    while(n--){
+        cin.getline(sentence,1000);
+        int len= strlen(sentence);
+        if(len>largest_len){
+            largest_len=len;
+            strcpy(largest,sentence);
+        }
+    }
+    cout<<"Largest string is "<<largest<<endl;
+   
+	return 0;
+}
