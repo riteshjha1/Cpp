52b0ac4604fd8d8112eb86bc8f7975d08d283d53
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2022-04-19 23:13:08 +0530

    Time: 20 ms (74.42%), Space: 24.2 MB (61.00%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,26 +11,34 @@
  */
 
 /*
-Iterative
+Approach :
+Inorder traversal of BST print the element in increasing order so that kth smallest element in BST is the kth item in inorder traversal
+
+Recursive
 Time complexcity O(H+K) H is height of bst
-Space complexcity O(H+K)
+Space complexcity O(1)
+hence we are not using any extra space but for the recursion calls space is allocated and that would be in order of O(H+K) (Recursion stack space)
 */
 class Solution {
 public:
-   int kthSmallest(TreeNode* root, int k) {
-        stack<TreeNode *> stk;
-        while(1){
-            while(root!=NULL){
-                stk.push(root);
-                root=root->left;
-            }
-            root=stk.top();
-            stk.pop();
-            k--;
-            if(k==0)
-                return root->val;
-            root=root->right;
-        }
+   int kthSmallest(TreeNode* root, int &k) {
+        if(root==NULL) 
+            return -1;
+        
+        int left = kthSmallest(root->left,k);          // left recursive call
+        
+        if(left!=-1) 
+            return left;                      // if we get answer from left call than return it
+        k--;
+        if(k==0) 
+            return root->val;                 // this is kth smallest element
+         
+        int right = kthSmallest(root->right,k);     // right recursive call
+        
+        if(right!=-1)
+            return right;                   // if we get answert from right call than return it
+        
+        return -1;                                 // return -1 if we not get answer from this call
     }
 };
 
