3ef05373f96f07090cc5a093116a8978521276f3
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2022-04-29 15:38:18 +0530

    Time: 3 ms (98.55%), Space: 7.3 MB (47.26%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> arr(26);
+        
+        for(auto &it : s) 
+            arr[it - 'a']++;
+        
+        for(auto &it : t) 
+            arr[it - 'a']--;
+        
+        for(auto &it : arr) 
+            if(it) 
+                return 0;
+        
+        return 1;
+    }
+};
