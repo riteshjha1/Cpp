a09162b6393965b7f6490842f7141b4e83452790
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2022-05-11 23:21:58 +0530

    Time: 16 ms (63.82%), Space: 18.7 MB (5.90%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,53 +1,52 @@
-/*Algorthem:
-
-initialize left pointer to 0 and right pointer to 1
-if height of right >= height of left
-check the water contained in between
-let left and right to its new poistion
-else
-increase right by one
-finally if these is open end
-e.g. [4, 0, 3]
-check the water backward in between.
-*/    
+// using stack solution
 
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // Two Pointer
-        int left = 0;
-        int right = 1;
-        int ans = 0;
-        while (right < height.size()) {
-            // right height >= left height, check water in between
-            if (height[right] >= height[left]) {
-                for (int i = left + 1; i < right; ++i) {
-                    ans += (height[left] - height[i]);
+        stack<int> stk, stk_2;
+        int l = height[0], trapped = 0;
+        stk.push(height[0]);
+        
+        for (int i=1; i<height.size(); i++) {
+            if (height[i]>=l) {
+                while (stk.top() != l) {
+                    trapped += (l-stk.top());
+                    stk.pop();
                 }
-                left = right;
-                ++right;
-            } else {
-                ++right;
+                stk.pop();
+                stk.push(height[i]);
+                l = height[i];
             }
+            else {
+                stk.push(height[i]);
+            }
+        }     
+        
+        if (!stk.empty()) {
             
-        }
-        // finally, when there is a open end, check from back.
-        if (left != right + 1) { 
-            int l = height.size() - 2;
-            int r = l + 1;
-            while ( l >= left) {
-                if (height[l] >= height[r]) {
-                    for (int i = r - 1; i > l; --i) {
-                        ans += (height[r] - height[i]);
+            l = stk.top();
+            stk.pop();
+            stk_2.push(l);
+            
+            while (!stk.empty()) {
+                int top = stk.top();
+                stk.pop();
+
+                if (top>=l) {
+                    while (stk_2.top() != l) {
+                        trapped += (l-stk_2.top());
+                        stk_2.pop();
                     }
-                    r = l;
-                    --l;
-                } else {
-                    --l;
+                    stk_2.pop();
+                    stk_2.push(top);
+                    l = top;
                 }
-            }
+                else {
+                    stk_2.push(top);
+                }
+            }            
         }
         
-        return ans;
+        return trapped;
     }
 };
