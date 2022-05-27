0b9c6d59d052008182d331ec72891294813daa24
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2022-05-27 17:07:59 +0530

    Time: 7 ms (30.37%), Space: 5.8 MB (74.53%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -3,8 +3,7 @@ public:
     int mySqrt(int x) {
         if (x <= 1)
             return x;
-        if (x < 0)
-            return INT_MAX;
+        
         int low = 0, high = x, res;
         while (low <= high) {
             int mid = low + (high - low) / 2;
