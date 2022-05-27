65630fdda1f1b71e539f47682a644878b6141b65
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2022-05-27 23:12:44 +0530

    Time: 20 ms (56.15%), Space: 16 MB (26.67%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    char nextGreatestLetter(vector<char>& letter, char t){
-        int h=letter.size()-1;
-        int l=0;
-        while(h>=l){
-            int mid=l+(h-l)/2;
-            if(letter[mid]>t)
-                h=mid-1;
+	// This helper does binary search to find floor value of the given char for instance {97, 99, 102 , 106} key = 100 will return 99 in other words {'a','c','f','j' }, key = 'd' former array is just ascii versions of the chars array. 
+    int helper(vector<char>& A, char key){
+        int l=0, r= A.size();
+        int m;
+        while(l+1<r){
+            m = l + (r-l)/2;
+            if(A[m]<=key)
+                l = m;
             else
-                l=mid+1;
+                r= m;
         }
-        if(l==letter.size())
-            return letter[0];
-        return letter[l];
+        return l;
+    }
+    char nextGreatestLetter(vector<char>& A, char key) {
+        if(key< A[0]) // check if key is even smaller than the first elem of the array in which case we'll return the first elem
+            return A[0];
+        int res = helper(A,key); 
+        return res < A.size()-1 ? A[res+1] :  A[0];
+             
     }
 };
