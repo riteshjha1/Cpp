c37657df81f54869e799d81ed3fb5fed8b555848
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2022-05-29 10:54:53 +0530

    Time: 121 ms (38.05%), Space: 15.7 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,32 +1,34 @@
 /* 
-The Second approach (Using Bitmasking)
-So, here we basically just find hash for each word and if we do AND (&) of hash of two words and it becomes equal to 0, then defintely they contain no common letter, so we update ans to max(size(word[i])size(word[j]))
-Using Bitmasking there is not much a difference in time complexity but the time to iterate over the bitset in approach I is reduced to constant O(26)
+So, basically we need to find all the pairs of words which has no common letters in them and then find the max(size(word[i])size(word[j]))
 
-Time Complexity : O(n*n)
+✅ The first approach is pretty straight forward (Using Bitset/Hashmap)
+So, we create a function named checkCommon to check if the letter is present in both the words or not and return true/false depending on the given words.
+And then we just iterate over the map and if there's any letter that occurs in both the words and if it does, we know that the word have atleast 1 letter in common.
+
+We will be using bitset<26> we could have used hashmap as well but bitset will help in the space optimizing so we are using that.
+
+Time Complexity : O(n*n) , where n is the length of words
 Space Complexity :O(n)
 */
 
-
-
-
-
-
-
 class Solution {
 public:
-    int maxProduct(vector<string>& words) {     // function to get the max product of two words
-	int n = words.size(), ans = 0;       // n is the number of words and ans is the max product
-	
-    vector<int> mask(n);               // mask is the vector of bit masks
-	for(int i = 0; i < n; i++) {      // loop to get the bit mask of each word
-		for(auto& ch : words[i])     // for each character in the word          
-			mask[i] |= 1 << (ch - 'a');   // hash the word
-		
-        for(int j = 0; j < i; j++) // loop to check if the word is a subset of another word
-			if((mask[i] & mask[j]) == 0)    // if no common set bit is found in the hash of words[i] and words[j]
-				ans = max(ans, int(size(words[i]) * size(words[j]))); // update the max product
-	}    
-	return ans; // return the max product
-}
+    bool checkCommon(bitset<26> &a, bitset<26> &b){ // function to check if two bitset are common
+        for(int i=0;i<26;i++) if(a[i] && b[i]) return true; // if any of the bits are true, return true
+        return false; // otherwise return false
+    }
+   
+    int maxProduct(vector<string>& words) { // function to find the maximum product
+        int n = words.size(); // number of words
+        int ans=0; // initialize the answer
+        vector<bitset<26>> chars(n); // vector of bitset
+        for(int i=0;i<n;i++){ // iterate over all the words 
+            for(auto &ch:words[i]) // iterate over all the characters in the words[]
+                chars[i][ch-'a'] =1; // set the bitset to 1
+            for(int j=0;j<i;j++) // iterate over all the words before the current word
+                if(!checkCommon(chars[i],chars[j])) // if the two words are not common
+                    ans = max(ans, (int)words[i].size()*(int)words[j].size()); // update the answer
+        }
+        return ans; // return the answer
+    }
 };
