02da3ab52d1b802ebfff08beb27541c0b625ef29
RITESH JHA <84015209+riteshjha1@users.noreply.github.com>
2022-06-08 23:45:15 +0530

    Time: 41 ms (65.28%), Space: 10.6 MB (71.34%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+        int firstUniqChar(string s) {
+        for(int i=0;i<s.size();i++){
+            if(s.find(s[i])==s.find_last_of(s[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
